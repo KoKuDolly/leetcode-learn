fc9f8a22fdffcf9923342a61a528942b8e71aa2b
kokudolly <1226838252@qq.com>
2023-04-17 13:25:58 +0800

    update 200

--- 200.岛屿数量.js
+++ 200.岛屿数量.js
@@ -4,7 +4,7 @@
  * [200] 岛屿数量
  */
 
-// @lc code=start
+//　@lc code=start
 
 // DFS
 // var dfs = function(grid, r, c) {
@@ -92,8 +92,8 @@ UnionFind.prototype.UnionFind = function(grid) {
 	for(let i = 0; i < m; i++) {
 		for(let j = 0; j < n; j++) {
 			if(grid[i][j] === '1') {
-				this.count++
 				this.parent[i * n + j] = i * n + j
+				this.count++
 			}
 			this.rank[i * n + j] = 0
 		}
@@ -126,7 +126,24 @@ UnionFind.prototype.getCount = function(){
 }
 
 var numIslands = function(grid) {
+	const nr = grid.length
+	if(!nr) return 0
+	const nc = grid[0].length
 
+	const uf = new UnionFind(grid)
+
+	for(let r = 0; r < nr; r++) {
+		for (let c = 0; c < nc; c++) {
+			if(grid[r][c] === '1') {
+				grid[r][c] = '0'
+				if(r - 1 >= 0 && grid[r - 1][c] === '1') uf.union(r * nc + c, (r - 1) * nc + c)
+				if(r + 1 < nr && grid[r + 1][c] === '1') uf.union(r * nc + c, (r + 1) * nc + c)
+				if(c - 1 >= 0 && grid[r][c - 1] === '1') uf.union(r * nc + c, r * nc + c - 1)
+				if(c + 1 < nc && grid[r][c + 1] === '1') uf.union(r * nc + c, r * nc + c + 1)
+			}
+		}
+	}
+
+	return uf.getCount()
 }
 // @lc code=end
-
